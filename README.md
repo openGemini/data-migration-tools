4cc3912b0f1c131ad4ffab5c8a0122744738031d
shilinlee <836160610@qq.com>
2023-06-06 20:07:21 +0800

    perf: import data optimization
    1. Min heap get min time
    2. Series is converted to mst, tag uses lru algorithm
    3. add readme on how to use batch flag
    
    Signed-off-by: shilinlee <836160610@qq.com>

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # data-migration-tool
 
 ## description
+
 The tool name is dataMigrate. It is used to migrate InfluxDB data to openGemini.
 The dataMigrate directly reads data from the TSM file of InfluxDB and writes the data to openGemini.
 
 ## quick start
 
 ### requirements
-Go version >1.16  
+
+Go version >1.16
 
 Setting Environment Variables
+
 ```
 > export GOPATH=/path/to/dir
 > export GO111MODULE=on
@@ -18,11 +21,13 @@ Setting Environment Variables
 ```
 
 ### compile
+
 ```
 > bash build.sh
 ```
 
 ### data migration
+
 ```
 > dataMigrate --from path/to/tsm-file --to ip:port --database dbname
 ```
@@ -40,12 +45,16 @@ Usage: dataMigrate [flags]
     	Optional: the retention policy to read (requires -database)
   -start string
     	Optional: the start time to read (RFC3339 format)
+  -batch int
+    	Optional: specify batch size for inserting lines (default 1000)
   -mode string
       Optional: whether to enable debug log or not (set as "Debug" to enable it)
   -to string
     	Destination host to write data to (default "127.0.0.1:8086",which is the openGemini service default address)
 ```
 
-**Notice**: When using this tool, please do not migrate data without shutting down InfluxDB if possible;   otherwise, some unknown problems may occur.   To ensure that data is as complete as possible after migration, keep the empty write load running before shutting down InfluxDB and wait for data in the cache to complete disk dumping (10 minutes by default).
+**Notice**: When using this tool, please do not migrate data without shutting down InfluxDB if possible; otherwise, some
+unknown problems may occur. To ensure that data is as complete as possible after migration, keep the empty write load
+running before shutting down InfluxDB and wait for data in the cache to complete disk dumping (10 minutes by default).
 
 **Welcome to add more features.**
--- src/log.go
+++ src/log.go
@@ -5,105 +5,105 @@ copyright 2023 Qizhi Huang(flaggyellow@qq.com)
 package main
 
 import (
-    "log"
-    "os"
-    "path/filepath"
-    "time"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
 )
 
 const (
-    TOCONSOLE = 1 << iota
-    TOLOGFILE
+	TOCONSOLE = 1 << iota
+	TOLOGFILE
 )
 
 const (
-    LEVEL_DEBUG = iota
-    LEVEL_INFO
-    LEVEL_WARNING
-    LEVEL_ERROR
+	LEVEL_DEBUG = iota
+	LEVEL_INFO
+	LEVEL_WARNING
+	LEVEL_ERROR
 )
 
 var LevelPrefixDict map[int]string = map[int]string{
-    LEVEL_INFO:    "INFO: ",
-    LEVEL_DEBUG:   "DEBUG: ",
-    LEVEL_WARNING: "WARNING: ",
-    LEVEL_ERROR:   "ERROR: ",
+	LEVEL_INFO:    "INFO: ",
+	LEVEL_DEBUG:   "DEBUG: ",
+	LEVEL_WARNING: "WARNING: ",
+	LEVEL_ERROR:   "ERROR: ",
 }
 
 type Logger struct {
-    logDir        string
-    logName       string
-    fileWriter    *os.File
-    fileLogger    *log.Logger
-    consoleLogger *log.Logger
-    errorLogger   *log.Logger
-    debug         bool
+	logDir        string
+	logName       string
+	fileWriter    *os.File
+	fileLogger    *log.Logger
+	consoleLogger *log.Logger
+	errorLogger   *log.Logger
+	debug         bool
 }
 
 func NewLogger() *Logger {
-    var err error
-    tm := time.Unix(0, time.Now().UnixNano())
-    timestr := tm.Format("2006-01-02_15-04-05")
-    filename := "migrate_log_" + timestr + ".log"
-    l := &Logger{
-        logDir:  "./logs",
-        logName: filename,
-        debug:   false,
-    }
-    logPath := filepath.Join(l.logDir, l.logName)
+	var err error
+	tm := time.Unix(0, time.Now().UnixNano())
+	timestr := tm.Format("2006-01-02_15-04-05")
+	filename := "migrate_log_" + timestr + ".log"
+	l := &Logger{
+		logDir:  "./logs",
+		logName: filename,
+		debug:   false,
+	}
+	logPath := filepath.Join(l.logDir, l.logName)
 
-    err = os.MkdirAll(l.logDir, os.ModePerm)
-    if err != nil {
-        log.Fatalf("ERROR: failed to create log path: %v\n", err)
-    }
+	err = os.MkdirAll(l.logDir, os.ModePerm)
+	if err != nil {
+		log.Fatalf("ERROR: failed to create log path: %v\n", err)
+	}
 
-    l.fileWriter, err = os.OpenFile(logPath, os.O_WRONLY|os.O_CREATE, 0755)
-    if err != nil {
-        log.Fatalf("ERROR: failed to open log file: %v\n", err)
-    }
+	l.fileWriter, err = os.OpenFile(logPath, os.O_WRONLY|os.O_CREATE, 0755)
+	if err != nil {
+		log.Fatalf("ERROR: failed to open log file: %v\n", err)
+	}
 
-    l.fileLogger = log.New(l.fileWriter, "", log.LstdFlags)
-    l.consoleLogger = log.New(os.Stdout, "", log.LstdFlags)
-    l.errorLogger = log.New(os.Stderr, "\n", 0)
+	l.fileLogger = log.New(l.fileWriter, "", log.LstdFlags)
+	l.consoleLogger = log.New(os.Stdout, "", log.LstdFlags)
+	l.errorLogger = log.New(os.Stderr, "\n", 0)
 
-    return l
+	return l
 }
 
 func (l *Logger) SetDebug() {
-    l.debug = true
+	l.debug = true
 }
 
 func (l *Logger) IsDebug() bool {
-    return l.debug
+	return l.debug
 }
 
 func (l *Logger) LogString(str string, target int, level int) {
-    if level == LEVEL_DEBUG {
-        if !l.debug {
-            return
-        }
-    }
-    prefix := LevelPrefixDict[level]
-    if target&TOLOGFILE > 0 {
-        l.fileLogger.Println(prefix, str)
-    }
-    if target&TOCONSOLE > 0 {
-        if level < LEVEL_WARNING {
-            l.consoleLogger.Println(str)
-        } else if level >= LEVEL_WARNING {
-            l.consoleLogger.Println(prefix, str)
-        }
-    }
+	if level == LEVEL_DEBUG {
+		if !l.debug {
+			return
+		}
+	}
+	prefix := LevelPrefixDict[level]
+	if target&TOLOGFILE > 0 {
+		l.fileLogger.Println(prefix, str)
+	}
+	if target&TOCONSOLE > 0 {
+		if level < LEVEL_WARNING {
+			l.consoleLogger.Println(str)
+		} else if level >= LEVEL_WARNING {
+			l.consoleLogger.Println(prefix, str)
+		}
+	}
 }
 
 func (l *Logger) LogError(err error) {
-    l.fileLogger.Println("ERROR: ", err.Error())
-    l.errorLogger.Println("ERROR: ", err)
+	l.fileLogger.Println("ERROR: ", err.Error())
+	l.errorLogger.Println("ERROR: ", err)
 }
 
 func (l *Logger) close() {
-    l.fileLogger = nil
-    l.fileWriter.Close()
-    l.consoleLogger = nil
-    l.errorLogger = nil
+	l.fileLogger = nil
+	l.fileWriter.Close()
+	l.consoleLogger = nil
+	l.errorLogger = nil
 }
